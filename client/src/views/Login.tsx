76cef86f1768d6d5cdc5a8b6f7e815fd28dc41e6
MarkCheng <mocheng26@gmail.com>
2021-03-20 19:41:42 +0800

    feat: form integrity

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,8 +1,9 @@
 import { Button, TextField } from '@material-ui/core'
-import React, { ReactElement, useState } from 'react'
+import React, { ChangeEvent, ReactElement, useState } from 'react'
 import styled from 'styled-components'
 import { COLOR } from '../components/GlobalStyle'
 import { MEDIA_BREAK } from '../components/Layout'
+import { checkIntegrity, VALIDATORS } from '../formIntegrity'
 
 type Props = {
   children?: ReactElement
@@ -10,10 +11,6 @@ type Props = {
 
 const Login = (props: Props): ReactElement => {
   const { children, ...rest } = props
-  const [input, setInput] = useState({
-    username: { value: '', errMsg: '' },
-    password: { value: '', errMsg: '' },
-  })
 
   const [isLogin, setIsLogin] = useState(true)
 
@@ -28,13 +25,47 @@ type FormProps = {
 
 const LoginForm = (props: FormProps) => {
   const { setIsLogin } = props
+  const [input, setInput] = useState({
+    username: { value: '', errMsg: '' },
+    password: { value: '', errMsg: '' },
+  })
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const nextState = input
+    console.log(e)
+    nextState[e.target.id as keyof typeof input] = { value: e.target.value, errMsg: '' }
+    setInput({ ...nextState })
+  }
+
+  const handleSubmit = () => {
+    const username = checkIntegrity(input.username, [VALIDATORS.REQUIRED])
+    const password = checkIntegrity(input.password, [VALIDATORS.REQUIRED])
+    setInput({ ...input, username, password })
+  }
+
   return (
     <Form>
       <Title>Welcome!</Title>
-      <TextInput id="username" label="username" variant="outlined" />
-      <TextInput id="password" label="password" variant="outlined" />
+      <TextInput
+        id="username"
+        label="username"
+        variant="outlined"
+        onChange={handleInputChange}
+        error={!!input.username.errMsg}
+        helperText={input.username.errMsg}
+      />
+      <TextInput
+        id="password"
+        label="password"
+        variant="outlined"
+        onChange={handleInputChange}
+        error={!!input.password.errMsg}
+        helperText={input.password.errMsg}
+      />
       <Link>Forget your password?</Link>
-      <Btn variant="contained">Login</Btn>
+      <Btn variant="contained" onClick={handleSubmit}>
+        Login
+      </Btn>
       <Details>
         Need an account? <Link onClick={() => setIsLogin(false)}>Sign up</Link>
       </Details>
@@ -44,13 +75,55 @@ const LoginForm = (props: FormProps) => {
 
 const SignupForm = (props: FormProps) => {
   const { setIsLogin } = props
+  const [input, setInput] = useState({
+    email: { value: '', errMsg: '' },
+    username: { value: '', errMsg: '' },
+    password: { value: '', errMsg: '' },
+  })
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const nextState = input
+    nextState[e.target.id as keyof typeof input] = { value: e.target.value, errMsg: '' }
+    setInput({ ...nextState })
+  }
+
+  const handleSubmit = () => {
+    const email = checkIntegrity(input.email, [VALIDATORS.REQUIRED, VALIDATORS.EMAIL])
+    const username = checkIntegrity(input.username, [VALIDATORS.REQUIRED])
+    const password = checkIntegrity(input.password, [VALIDATORS.REQUIRED])
+    setInput({ ...input, email, username, password })
+  }
+
   return (
     <Form>
       <Title>Create a new account</Title>
-      <TextInput id="email" label="email" variant="outlined" />
-      <TextInput id="username" label="username" variant="outlined" />
-      <TextInput id="password" label="password" variant="outlined" />
-      <Btn variant="contained">Login</Btn>
+      <TextInput
+        id="email"
+        label="email"
+        variant="outlined"
+        onChange={handleInputChange}
+        error={!!input.email.errMsg}
+        helperText={input.email.errMsg}
+      />
+      <TextInput
+        id="username"
+        label="username"
+        variant="outlined"
+        onChange={handleInputChange}
+        error={!!input.username.errMsg}
+        helperText={input.username.errMsg}
+      />
+      <TextInput
+        id="password"
+        label="password"
+        variant="outlined"
+        onChange={handleInputChange}
+        error={!!input.password.errMsg}
+        helperText={input.password.errMsg}
+      />
+      <Btn variant="contained" onClick={handleSubmit}>
+        Login
+      </Btn>
       <Link onClick={() => setIsLogin(true)}>Already have an account?</Link>
     </Form>
   )
