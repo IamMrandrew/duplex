b76b73844b991f622c3ce924fae45062b23074af
MarkCheng <mocheng26@gmail.com>
2021-04-06 16:12:13 +0800

    fix: add cookie-parser

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -12,8 +12,8 @@ declare global {
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
   jwt.verify(req.cookies?.token, process.env.JWT_TOKEN || '', async (verifyErr: any, decoded: any) => {
-    if (verifyErr.message == 'jwt expired') return sendError(res, 401, 'Token expried', verifyErr)
-    if (verifyErr.message == 'jwt must be provided') return sendError(res, 401, 'Unauthorized', verifyErr)
+    if (verifyErr?.message == 'jwt expired') return sendError(res, 401, 'Token expried', verifyErr)
+    if (verifyErr?.message == 'jwt must be provided') return sendError(res, 401, 'Unauthorized', verifyErr)
     if (decoded) {
       req.userData = decoded
       next()
--- server/middleware/index.ts
+++ server/middleware/index.ts
@@ -1,16 +1,16 @@
 // middleware that are applicable to all routes
 import express, { Router } from 'express'
 // // import passport from 'passport'
-import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
 const middleware = Router()
 // // middlewares.use(passport.initialize())
 // // middlewares.use(passport.session())
-
+middleware.use(cookieParser())
 middleware.use(express.json())
 middleware.use(express.urlencoded({ extended: false }))
-middleware.use(express.json())
+
 middleware.use(cors({ credentials: true, origin: true }))
 
 export default middleware
